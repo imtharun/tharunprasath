409649369c035413967341b2a5116a7149ce61b0
Tharunprasath A S <80610149+imtharun@users.noreply.github.com>
2023-02-13 13:06:23 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Suspense fallback={<LoadingSpinner />}>
       <Routes>
         <Route
-          path="/"
+          path="/tharunprasath"
           element={
             <main className="relative min-h-screen gradient">
               <Header screen="home" />
