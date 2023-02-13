d06a69a88b8020a6bacab312ff3ac942d4286d46
Tharunprasath A S <80610149+imtharun@users.noreply.github.com>
2023-02-13 12:53:41 +0530

    fix: bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from "react";
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoadingSpinner from "./components/LoadingSpinner";
 
 const Header = lazy(() => import("./components/Header"));
@@ -11,38 +11,36 @@ const Error = lazy(() => import("./components/Error"));
 function App() {
   return (
     <Suspense fallback={<LoadingSpinner />}>
-      <HashRouter basename="/">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <main className="relative min-h-screen gradient">
-                <Header screen="home" />
-                <Main />
-              </main>
-            }
-          />
-          <Route
-            path="/skills"
-            element={
-              <main className="relative min-h-screen gradient">
-                <Header screen="" />
-                <Skills />
-              </main>
-            }
-          />
-          <Route
-            path="/projects"
-            element={
-              <main className="relative min-h-screen bg-light">
-                <Header screen="others" />
-                <Projects />
-              </main>
-            }
-          />
-          <Route path="/*" element={<Error />} />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main className="relative min-h-screen gradient">
+              <Header screen="home" />
+              <Main />
+            </main>
+          }
+        />
+        <Route
+          path="/skills"
+          element={
+            <main className="relative min-h-screen gradient">
+              <Header screen="" />
+              <Skills />
+            </main>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <main className="relative min-h-screen bg-light">
+              <Header screen="others" />
+              <Projects />
+            </main>
+          }
+        />
+        <Route path="/*" element={<Error />} />
+      </Routes>
     </Suspense>
   );
 }
